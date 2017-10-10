75dcc3a93341356c874597131cd768b8ab5c1384
Użytkownik Gość <Guest@imac-07.local>
2017-10-10 10:52:59 +0200

    added main storyboard, requesting data on initialization, and some initial controller stuff

--- AlbumBrowser/ViewController.swift
+++ AlbumBrowser/ViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var albumData = [String: Any]()
+    var currentAlbum: String? = ""
+    
+    @IBOutlet weak var recordTitle: UILabel!
 
+    @IBOutlet weak var wykonawca: UITextField!
+    @IBOutlet weak var tytul: UITextField!
+    @IBOutlet weak var gatunek: UITextField!
+    @IBOutlet weak var rokWydania: UITextField!
+    @IBOutlet weak var liczbaSciezek: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        requestInitialData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +33,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    private func requestInitialData() {
+        let session = URLSession.shared
+        let url = URL.init(string: "https://isebi.net/albums.php")
+        
+        session.dataTask(with: url!, completionHandler: {
+            (maybeData: Data?, response: URLResponse?, err: Error?) in
+            
+            guard let data = maybeData else { print("NI MA"); return }
+            let thing = try? JSONSerialization.jsonObject(with: data)
+            guard let foo = thing else { print("NI MA"); return }
+            self.albumData = foo as! [String: Any]
+            DispatchQueue.main.async {
+//                self.currentAlbum = self.albumData.first.key
+                
+            }
+
+        })
+    }
+    
+    
+    
 
 }
 
